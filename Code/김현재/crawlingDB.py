c13638f6f686dc5e4e2180679559dbf86a97f069
KHJae <48306079+KHJae@users.noreply.github.com>
2020-04-20 05:49:54 +0900

    크롤링한 정보 DB

--- Code/김현재/crawlingDB.py
+++ Code/김현재/crawlingDB.py
@@ -14,7 +14,6 @@ class DB :
 
     def insert(self, name, index, i) :
         day = time.strftime('%Y-%m-%d %H:%M', time.localtime(time.time()))
-        
         sql = 'INSERT INTO stock(name, day, current, tran, start, high, low) VALUES (\'%s\', \'%s\', %s, %s, %s, %s, %s);' %(name, day, index[i], index[i+1], index[i+2], index[i+3], index[i+4])
         print(sql)
         self.cursor.execute(sql)
