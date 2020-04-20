56ea1c565e1f7d293a489b6bf3497acb82e94a1c
KHJae <48306079+KHJae@users.noreply.github.com>
2020-04-21 01:38:01 +0900

    [DataBase] 크롤링 주식정보 삽입

--- Code/김현재/crawlingDB.py
+++ Code/김현재/crawlingDB.py
@@ -1,5 +1,4 @@
 import pymysql
-import time
 
 class DB :
     def __init__(self) :
@@ -12,9 +11,7 @@ class DB :
         )
         self.cursor = self.conn.cursor()
 
-    def insert(self, name, index, i) :
-        day = time.strftime('%Y-%m-%d %H:%M', time.localtime(time.time()))
+    def insert(self, name, index, i, day) :
         sql = 'INSERT INTO stock(name, day, current, tran, start, high, low) VALUES (\'%s\', \'%s\', %s, %s, %s, %s, %s);' %(name, day, index[i], index[i+1], index[i+2], index[i+3], index[i+4])
-        print(sql)
         self.cursor.execute(sql)
         self.conn.commit()
