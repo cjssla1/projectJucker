6f74c510e830dea45e7bf093c15be3afe76b6bfb
Jinyong Kim <w987987@naver.com>
2020-04-29 03:12:25 +0900

    레이아웃 이동 코드

---
+++ Code/김진용/Scripts/MainUImanager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainUImanager : MonoBehaviour
+{
+    public void stockClick()
+    {
+        Debug.Log("2_StockUp called");
+        SceneManager.LoadScene("2_StockUp");
+    }
+    public void boardClick()
+    {
+        Debug.Log("4_BoardNormal called");
+        SceneManager.LoadScene("4_BoardNormal");
+    }
+}
---
+++ Code/김진용/Scripts/StockUpUImanager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StockUpUImanager : MonoBehaviour
+{
+    public void downClick()
+    {
+        Debug.Log("3_StockDown called");
+        SceneManager.LoadScene("3_StockDown");
+    }
+
+    public void boardClick()
+    {
+        Debug.Log("4_BoardNormal called");
+        SceneManager.LoadScene("4_BoardNormal");
+    }
+
+    public void backClick()
+    {
+        Debug.Log("1_Main called");
+        SceneManager.LoadScene("1_Main");
+    }
+}
