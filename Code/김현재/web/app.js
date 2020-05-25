b0f34ad0088b009a0144d7c58630bc97a39754b1
KHJae <48306079+KHJae@users.noreply.github.com>
2020-05-25 13:34:10 +0900

    20-05-25 app.js

--- Code/김현재/web/app.js
+++ Code/김현재/web/app.js
@@ -7,7 +7,7 @@ const mysql = require("mysql");
 
 const client = mysql.createConnection({
     host: "localhost",
-    user: "admin",
+    user: "root",
     password: "3946",
     database: "mydb"
 });
@@ -25,29 +25,105 @@ app.use(bodyParser.json());
 
 app.get('/', function (req, res){
     console.log("hello");
-    client.query("select name, current, aggregate, tran from stock where date_format(day, '%Y-%m-%d') = date_format(now(), '%Y-%m-%d') order by aggregate DESC limit 10;", function(err, result, fields){
+    client.query("select name, current, aggregate, tran, updown from stock where date_format(day, '%Y-%m-%d') = date_format(now(), '%Y-%m-%d') order by aggregate DESC limit 10;", function(err, result, fields){
         if (err){
-            console.log("error");
+            console.log(err + "error1");
         }
         else {
-            res.render("main", {
-                results: result
+            client.query("select name, updown from stock where updown >= 5 and date_format(day, '%Y-%m-%d') = date_format(now(), '%Y-%m-%d') order by updown DESC limit 10;", function(err, result2, fields){
+                if (err){
+                    console.log("중첩에러");
+                }
+                else{
+                    client.query("select idx, title, cnt_comment from board where likeno >= 3 order by idx DESC limit 10;", function(err, result3, fields) {
+                        if(err){
+                            console.log(err);
+                        }
+                        else {
+                            res.render("main", {
+                                results: result, //실시간 주가 정보
+                                results2: result2, // 급등 종목 정보
+                                results3: result3
+                            });
+                        }
+                    })
+                }
             });
+            
+            //res.render("main", {
+            //    results: result
+            //});
         }
     });
+})
+
+app.post('/', function(req, res) {
+    var Vname = req.body.stock_name;
+    //console.log('POST Para = ' + Vname);
+    client.query("select current from stock where name = ? and date_format(day, '%Y-%m-%d') = date_format(now(), '%Y-%m-%d')", [Vname], function(err, result, fieldds){
+        if (err){
+            console.log("err");
+        }
+        else {
+            var Vprice = result[0].current;
+            var arr = new Array(Vname, Vprice);
+            console.log(arr);
+            res.send({result:arr});
+        }
+    })
+    //console.log("zz"+Vname);
+    //res.send({result:Vname})
+
 })
 
 app.get('/board', function (req, res){
     console.log("hello");
     
-    client.query("select * from board order by idx desc limit 0,10;", function(err, result, fields){
+    client.query("select *, date_format(date, '%H:%i') as time from board order by idx desc limit 0,10;", function(err, result, fields){
         if (err){
-            console.log("error");
+            console.log("error2");
         }
         else {
-            res.render("board", {
-                results: result
-            });
+            client.query("select board_idx, count(*) AS cnt from comment group by board_idx;", function(err, result2, fields){
+                if (err){
+                    console.log("error2");
+                }
+                else{
+                    res.render("board", {
+                        results: result,
+                        results2: result2
+                    });
+                }
+            })
+            //res.render("board", {
+            //    results: result
+            //});
+        }
+    });
+})
+
+app.get('/recommend-board', function (req, res){
+    console.log("hello");
+    
+    client.query("select *, date_format(date, '%H:%i') as time from board where likeno >= 3 order by idx desc limit 0,10;", function(err, result, fields){
+        if (err){
+            console.log(err);
+        }
+        else {
+            client.query("select board_idx, count(*) AS cnt from comment group by board_idx;", function(err, result2, fields){
+                if (err){
+                    console.log("error2");
+                }
+                else{
+                    res.render("recommend-board", {
+                        results: result,
+                        results2: result2
+                    });
+                }
+            })
+            //res.render("board", {
+            //    results: result
+            //});
         }
     });
 })
@@ -55,14 +131,25 @@ app.get('/board', function (req, res){
 app.get('/view/:idx', function(req, res){
     console.log(req.params.idx);
        
-    client.query("select * from board where idx = ?", [req.params.idx], function(err, result, fields){
+    client.query("select *, date_format(date, '%Y-%m%-%d %H:%i:%s') as time from board where idx = ?", [req.params.idx], function(err, result, fields){
         if (err){
-            console.log("error");
+            console.log("error3");
         }
         else {
-            res.render("view", {
-                results: result
-            });
+            client.query("select *, date_format(date, '%Y-%m%-%d %H:%i:%s') as c_time from comment where board_idx = ?", [req.params.idx], function(err, result2, fields){
+                if (err){
+                    console.log("error3");
+                }
+                else {
+                    res.render("view", {
+                        results: result,
+                        results2: result2
+                    })
+                }
+            })
+            //res.render("view", {
+            //    results: result
+            //});
             let u_hit = result[0].hit + 1;
             console.log('wwww',result)
             client.query("update board set hit = ? where idx = ?", [u_hit, req.params.idx], function(err, result, fields){
@@ -74,7 +161,72 @@ app.get('/view/:idx', function(req, res){
     });
 });
     
+app.post('/view/:idx', function(req, res) {
+    var view_idx = req.body.view_idx;
+    var msg = req.body.msg;
+    var user = req.body.dat_user;
+    var pw = req.body.dat_pw;
+    var content = req.body.dat_content;
     
+
+    if(msg=="up"){
+        client.query("update board set likeno = likeno + 1 where idx = ? ", [view_idx], function(err, result, fieldds){
+            if (err){
+                console.log("err");
+            }
+            else {
+                res.send({result:"success"})
+                //var Vprice = result[0].current;
+                //var arr = new Array(Vname, Vprice);
+                //console.log(result);
+                //res.send({result:arr});
+            }
+        })
+    }
+    else if(msg=="down"){
+        client.query("update board set likeno = likeno - 1 where idx = ? ", [view_idx], function(err, result, fieldds){
+            if (err){
+                console.log("err");
+            }
+            else {
+                res.send({result:"success"})
+                //var Vprice = result[0].current;
+                //var arr = new Array(Vname, Vprice);
+                //console.log(result);
+                //res.send({result:arr});
+            }
+        })
+    }
+    else if(user && pw && content){
+        //console.log(req.params.idx);
+        console.log(user, pw, content);
+        client.query("insert into comment(board_idx, name, pw, content, date) values (?, ?, ?, ?, now())", [req.params.idx, user, pw, content], function(err, result, fieldds){
+            if (err){
+                console.log(err);
+            }
+            else {
+                client.query("update board set cnt_comment = cnt_comment + 1 where idx = ? ", [req.params.idx], function(err, result, fieldds){
+                    if (err){
+                        console.log(err);
+                    }
+                    else {
+                        res.redirect(req.get('referer'));
+                    }
+                })
+                //res.redirect(req.get('referer'));
+                //res.send({result:"success"})
+                //var Vprice = result[0].current;
+                //var arr = new Array(Vname, Vprice);
+                //console.log(result);
+                //res.send({result:arr});
+            }
+        })
+       // res.redirect(req.get('referer'));
+    }
+
+
+})
+
 
 
 app.get('/writer', function (req, res){
@@ -87,9 +239,9 @@ app.post('/writer', function (req, res){
     const b_content = req.body.content;
     const b_pw = req.body.pw;
 
-    client.query("insert into board(name, pw, title, content, date) values(?, ?, ?, ?, date_format(now(), '%Y-%m-%d'))", [b_name, b_pw, b_title, b_content], function(err, result, fields){
+    client.query("insert into board(name, pw, title, content, date) values(?, ?, ?, ?, now())", [b_name, b_pw, b_title, b_content], function(err, result, fields){
         if (err){
-            console.log("error");
+            console.log("error4");
         }
         else {
             res.redirect("board")            }
@@ -100,7 +252,7 @@ app.post('/writer', function (req, res){
 app.get('/modify/:idx', function (req, res){
     client.query("select * from board where idx = ?", [req.params.idx], function(err, result, fields){
         if (err){
-            console.log("error");
+            console.log("error5");
         }
         else {
             res.render("modify", {
@@ -124,7 +276,7 @@ app.post('/modify/:idx', function(req, res){
 app.get('/delete/:idx', function (req, res){
     client.query("select * from board where idx = ?", [req.params.idx], function(err, result, fields){
         if (err){
-            console.log("error");
+            console.log("error6");
         }
         else {
             res.render("delete", {
