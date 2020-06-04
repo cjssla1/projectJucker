4d9e3c796f38caeb1e637b345b51793c188b54de
cjssla1 <89kkl@naver.com>
2020-06-04 22:33:17 +0900

    회원가입 페이지 추가

--- Code/김경중/Web-React/Routes.js
+++ Code/김경중/Web-React/Routes.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import Board from './boardpage/Board';
 import Rank from './rankpage/Rank';
 import ReadPage from './boardpage/ReadPage';
@@ -8,7 +8,8 @@ import Update from './boardpage/Update';
 import Login from './authpage/Login';
 import Signup from './authpage/Signup';
 import LogoutButton from './authpage/LogoutButton';
-
+import Notfound from './homepage/Notfound';
+import Home from './homepage/Home';
 
 class Routes extends Component{
 
@@ -33,7 +34,7 @@ class Routes extends Component{
         var auth1 = ''
         var auth2 = ''
         if(this.state.session){
-            auth1 = this.state.session.id
+            auth1 = this.state.session.nickname
             auth2 = <LogoutButton></LogoutButton>
         } else{
             auth1 = <Link to="/auth/login">Login</Link>
@@ -48,7 +49,8 @@ class Routes extends Component{
             <div>{auth2}</div>
             <div><Link to="/rank">주식랭킹</Link></div>
             <div><Link to="/board">게시판</Link></div>
-            
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
             <Route exact path="/auth/login" component={Login}></Route>
             <Route exact path="/auth/signup" component={Signup}></Route>
             <Route exact path="/rank" component={Rank}></Route>
@@ -56,6 +58,8 @@ class Routes extends Component{
             <Route exact path="/board/create" component={Create}></Route>
             <Route exact path="/board/read/:id" component={ReadPage}></Route>
             <Route exact path="/board/update/:id" component={Update}></Route>
+            <Route component={Notfound}></Route>
+          </Switch>
         </Router>
         </div>
         );
