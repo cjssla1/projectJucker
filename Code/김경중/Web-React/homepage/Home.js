733147460614cf9ed9079414f7babf8d24009de2
cjssla1 <89kkl@naver.com>
2020-06-04 22:32:24 +0900

    홈페이지, 오류 페이지 추가

---
+++ Code/김경중/Web-React/homepage/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <div className="NotFound">
+          Home page : 홈페이지입니다.
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ Code/김경중/Web-React/homepage/Notfound.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <div className="NotFound">
+          Not Found : 없는 페이지입니다.
+      </div>
+    );
+  }
+}
+
+export default App;
